08e049c5969bf50261e6631eaca27a8228700d3c
Manuja <manuja@gmail.com>
2025-03-11 03:55:50 +0530

    Asset model updated

--- backend/models/Asset.js
+++ backend/models/Asset.js
@@ -1,1 +1,32 @@
 const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const assetSchema = new Schema({
+    ID : {
+        type : String,
+        required : true
+    },
+    assetName : {
+        type : String,
+        required : true
+    },
+    assetType : {
+        type : String,
+        enum : ['Non-current', 'Current'],
+        required : true
+    },
+    purchaseDate : {
+        type : Date,
+        required : true
+    },
+    assetValue : {
+        type : Double,
+        required : true
+    }
+
+})
+
+const Asset = mongoose.model("Asset", assetSchema);
+
+module.exports = Asset;
