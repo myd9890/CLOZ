5c4c284e43ba24899b51f41ccb440dcca94ad57a
Manuja <manuja@gmail.com>
2025-03-13 15:49:49 +0530

    add update, delete, and get (by id) routes for assets

--- backend/routes/Assets.js
+++ backend/routes/Assets.js
@@ -33,4 +33,51 @@ router.route("/").get((req,res)=>{
     })
 })
 
+router.route("/update/:id").put(async(req,res) =>{
+    let assetID = req.params.id;
+    const {ID,assetName,assetType,purchaseDate,assetValue} = req.body;
+
+    const updateAsset = {
+        ID,
+        assetName,
+        assetType,
+        purchaseDate,
+        assetValue
+    }
+
+    const update = await Asset.findByIdAndUpdate(assetID,updateAsset).then(() =>{
+        
+    res.status(200).send({status : "Asset updated!"})
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status : "Error with updating data!"})
+    })
+})
+
+
+
+router.route("/delete/:id").delete(async(req,res) =>{
+    let assetID = req.params.id;
+
+    await Asset.findByIdAndDelete(assetID).then(() => {
+        res.status(200).send({status : "Asset deleted!"});
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({status : "Error with deleting data!"});
+    })
+})
+
+router.route("/get/:id").get(async(req,res) =>{
+    let assetID = req.params.id;
+    const asset = await Asset.findById(assetID).then(() => {
+        res.status(200).send({status : "Asset fetched!", asset})
+    }).catch((err) => {
+        console.log(err.message);
+        res.status(500).send({status : "Error with fetching data!", error : err.message});
+    })
+})
+
+
+
+
 module.exports = router;
